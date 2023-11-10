d3be741663cf017fd3b1e80d581be1189172c025
Kurra Suresh <kurrasuresh1994@outlook.com>
2023-11-10 10:39:58 +0530

    Added in Controllers

--- Core.BookStore/Core.BookStore/Program.cs
+++ Core.BookStore/Core.BookStore/Program.cs
@@ -5,9 +5,16 @@ namespace Core.BookStore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //Add Services which is required by the application
+            builder.Services.AddControllersWithViews();
+
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+
+            // app.MapGet("/", () => "Hello World!");
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
