8112d37b78d16a9d404757ed247e398846f8f31f
Kurra Suresh <kurrasuresh1994@outlook.com>
2023-11-24 09:07:18 +0530

    Custom Tag helper functionality is added

--- Core.BookStore/Core.BookStore/Helpers/CustomEmailTagHelper.cs
+++ Core.BookStore/Core.BookStore/Helpers/CustomEmailTagHelper.cs
@@ -4,5 +4,13 @@ namespace Core.BookStore.Helpers
 {
     public class CustomEmailTagHelper : TagHelper
     {
+        public string MyEmail { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{MyEmail}");
+            output.Attributes.Add("Id", "myemail");
+            output.Content.SetContent("Email");
+        }
     }
 }
